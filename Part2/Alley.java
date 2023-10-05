963df1a0c00c851d00faa589910e034bc8ad2635
Lars Refsgaard <1552887+LarsRefsgaard@users.noreply.github.com>
2023-10-05 16:07:18 +0200

    Use MultiAlley()

--- Part2/Alley.java
+++ Part2/Alley.java
@@ -10,7 +10,7 @@ public class Alley {
     }
 
     public static Alley create() {
-        return new MonoAlley(); // Change to use desired implementation
+        return new MultiAlley(); // Change to use desired implementation
     }
 
     /* Block until car no. may enter alley */
